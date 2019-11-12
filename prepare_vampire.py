84668d70c4216d79b2a472eb9e375f0b5ca3cc7a
Sam Gehman <sgehman@cs.uw.edu>
2019-11-12 12:20:05 -0800

    Fix vampire script

--- prepare_vampire.py
+++ prepare_vampire.py
@@ -5,17 +5,33 @@ import pandas as pd
 
 from constants import DATA_DIR, URLS_PICKLE, BIAS_PICKLE, TEXTS_DIR, VAMPIRE_DIR
 
+
+def write_data_file(df, f):
+    for i, row in tqdm(df.iterrows(), total=len(df)):
+        text_file = TEXTS_DIR / row.filename
+        out_dict = {
+            'text': text_file.read_text(),
+            'label': row.bias
+        }
+
+        json.dump(out_dict, f)
+        f.write('\n')
+
+
 allsides = pd.read_pickle(BIAS_PICKLE)
 urls = pd.read_pickle(URLS_PICKLE)
 urls_merged = urls.merge(allsides)
 
-for i, row in tqdm(urls_merged.iterrows(), total=len(urls_merged)):
-    text_file = TEXTS_DIR / row.filename
-    out_dict = {
-        'text': text_file.read_text(),
-        'label': row.bias
-    }
-    
-    out_file = VAMPIRE_DIR / text_file.with_suffix('.vampire.json').name
-    with out_file.open('w') as f:
-        json.dump(out_dict, f)
+data_shuffled = urls_merged.sample(frac=1)
+split_1 = int(0.8 * len(data_shuffled))
+split_2 = int(0.9 * len(data_shuffled))
+splits = [
+    (urls_merged[:split_1], VAMPIRE_DIR / 'bias-train.jsonl'),
+    (urls_merged[split_1:split_2], VAMPIRE_DIR / 'bias-dev.jsonl'),
+    (urls_merged[split_2:], VAMPIRE_DIR / 'bias-test.jsonl')
+]
+
+for df, path in splits:
+    tqdm.write(str(path) + '\n')
+    with path.open('a') as f:
+        write_data_file(df, f)
