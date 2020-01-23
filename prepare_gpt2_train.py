631f0a6f72ff32cd4e2584d9f87aa31510b269bd
Sam Gehman <sgehman@cs.uw.edu>
2020-01-23 15:59:52 -0800

    Clean up gpt preparation

--- prepare_gpt2_train.py
+++ prepare_gpt2_train.py
@@ -2,12 +2,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sqlalchemy import create_engine
 from pathlib import Path
+from util import load_text
 
 from constants import DATA_DIR, TEXTS_DIR, OUTPUT_DIR
 
 
 def save_finetune_data(df: pd.DataFrame, train_file: Path, val_file: Path):
-    text_data = df['id'].apply(lambda fn: (TEXTS_DIR / (fn + ".txt")).read_text())
+    text_data = df['id'].apply(lambda id: load_text(id + ".txt"))
     train, val = train_test_split(text_data, test_size=0.2, random_state=42)
 
     print('\n'.join(train), file=train_file.open('w'))
@@ -16,15 +17,17 @@ def save_finetune_data(df: pd.DataFrame, train_file: Path, val_file: Path):
 
 if __name__ == '__main__':
     # Create sql connection
-    database_path = DATA_DIR / 'small_perspective_api_responses.db'
+    database_path = DATA_DIR / 'perspective_api_responses.db'
     engine = create_engine(f'sqlite:///{database_path}', echo=False)
 
     # Load dataframe from sql
     df = pd.read_sql(
-        'select * from responses where toxicity < 0.1',
+        'select * from responses where toxicity < 0.05',
         con=engine
     )
 
+    print("Number of selected examples:", len(df))
 
-
-    # save_finetune_data(df, )
+    train_file = OUTPUT_DIR / 'finetune.train.txt'
+    val_file = OUTPUT_DIR / 'finetune.val.txt'
+    save_finetune_data(df, train_file, val_file)
