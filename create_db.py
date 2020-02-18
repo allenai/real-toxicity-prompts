707c2ba3a0a31e5efc4f7085db7b2c6fc9aff303
Sam Gehman <sgehman@cs.uw.edu>
2020-02-18 12:26:14 -0800

    Update db script with span scores

--- create_db.py
+++ create_db.py
@@ -1,13 +1,14 @@
 import json
 from itertools import islice
 from pathlib import Path
+from typing import List, Union
 
 import click
-from sqlalchemy import Column, Float, String
+from sqlalchemy import Column, Float, String, Integer, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from tqdm import trange
+from sqlalchemy.orm import sessionmaker, relationship
+from tqdm import trange, tqdm
 
 BATCH_SIZE = 100000
 LIMIT = None
@@ -18,8 +19,9 @@ Session = sessionmaker()
 
 class Response(Base):
     __tablename__ = 'responses'
+    span_scores = relationship('SpanScore')
 
-    id = Column(String, primary_key=True)
+    filename = Column(String, primary_key=True)
     insult = Column(Float)
     severe_toxicity = Column(Float)
     toxicity = Column(Float)
@@ -30,59 +32,100 @@ class Response(Base):
     threat = Column(Float)
 
     def __repr__(self):
-        return f"<Response<id={self.id}>"
+        return f"<Response<filename={self.filename}>"
 
 
-def create_response(response_file: Path) -> Response:
+class SpanScore(Base):
+    __tablename__ = 'span_scores'
+
+    filename = Column(String, ForeignKey('responses.filename'), primary_key=True)
+    attribute = Column(String, primary_key=True)
+    begin = Column(Integer, primary_key=True)
+    end = Column(Integer, primary_key=True)
+    value = Column(Float)
+
+    def __repr__(self):
+        return f"<SpanScore<filename={self.filename}, type={self.attribute}>"
+
+
+def create_rows(response_file: Path) -> List[Union[Response, SpanScore]]:
     with response_file.open() as f:
         response_json = json.load(f)
 
-    id = response_file.name.split('.')[0]
-    attributes = {k.lower(): v['summaryScore']['value'] for k, v in response_json['attributeScores'].items()}
+    rows = []
+    filename = response_file.name.split('.json')[0]
+    attribute_scores = response_json['attributeScores'].items()
 
-    return Response(id=id, **attributes)
+    summary_scores = {}
+    for attribute, scores in attribute_scores:
+        attribute = attribute.lower()
+
+        # Save summary score
+        assert scores['summaryScore']['type'] == 'PROBABILITY'
+        summary_scores[attribute] = scores['summaryScore']['value']
+
+        # Save span scores
+        for span_score_dict in scores['spanScores']:
+            assert span_score_dict['score']['type'] == 'PROBABILITY'
+            span_score = SpanScore(filename=filename,
+                                   attribute=attribute,
+                                   begin=span_score_dict['begin'],
+                                   end=span_score_dict['end'],
+                                   value=span_score_dict['score']['value'])
+            rows.append(span_score)
+
+    response = Response(filename=filename, **summary_scores)
+    rows.append(response)
+
+    return rows
+
 
 @click.command()
 @click.argument('database_path')
 @click.argument('responses_dir')
 def main(database_path: str, responses_dir: str):
+    responses_dir = Path(responses_dir)
+    if not responses_dir.is_dir():
+        raise click.FileError('Responses directory does not exist')
+
     engine = create_engine(f'sqlite:///{database_path}', echo=False)
 
-    # Create schema for Response
+    # Create schemas
+    tqdm.write('Creating schemas...')
     Response.metadata.create_all(engine)
+    SpanScore.metadata.create_all(engine)
 
     # Start session
     Session.configure(bind=engine)
     session = Session()
 
-    responses_dir = Path(responses_dir)
+    tqdm.write('Counting responses... ', end='')
     num_responses = sum(1 for _ in responses_dir.iterdir())
     responses_dir_iter = responses_dir.iterdir()
+    tqdm.write(f'total: {num_responses}')
 
+    tqdm.write(f'Batch size: {BATCH_SIZE}, limit: {LIMIT}')
+    tqdm.write(f'Reading responses into database...')
     try:
-        # Create a sample response
+        # Add scores to our database
         for i in trange(0, num_responses, BATCH_SIZE):
             # Break if we will exceed our limit
             if LIMIT and i + BATCH_SIZE >= LIMIT:
                 break
 
-            # Get batch of responses
-            responses = []
+            # Get batch of rows
+            rows = []
             for file in islice(responses_dir_iter, BATCH_SIZE):
-                # Skip chunked files
-                if "chunk" in file.name:
-                    continue
-
                 try:
-                    responses.append(create_response(file))
+                    rows.extend(create_rows(file))
                 except Exception as e:
                     print("Error while creating response:", e)
 
             # If there are no more responses, break
-            if not responses:
+            if not rows:
                 break
 
-            session.bulk_save_objects(responses)
+            session.bulk_save_objects(rows)
 
         session.commit()
     except Exception as e:
