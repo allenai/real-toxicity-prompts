1f99730913fe3e8738cd8580517b4a4a2ab801ab
Sam Gehman <sgehman@cs.uw.edu>
2020-03-13 11:46:09 -0700

    Working in class

--- utils/generation.py
+++ utils/generation.py
@@ -34,70 +34,71 @@ class GPT2Generator:
             model_name_or_path = str(model_name_or_path)
 
         # Set up device
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        n_gpu = torch.cuda.device_count()
-        set_seed(seed, n_gpu)
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # n_gpu = torch.cuda.device_count()
+        # set_seed(seed, n_gpu)
 
         # Initialize the model and tokenizer
-        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name_or_path, pad_token=self.STOP_TOKEN)
-        self.model = GPT2LMHeadModel.from_pretrained(model_name_or_path).to(self.device)
-        self.pad_token_id = self.tokenizer.encode(self.STOP_TOKEN)[0]
+        self.model = GPT2LMHeadModel.from_pretrained(model_name_or_path)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name_or_path, pad_token='<PAD>')
+        # self.model = GPT2LMHeadModel.from_pretrained(model_name_or_path).to(self.device)
+        # self.pad_token_id = self.tokenizer.encode(self.STOP_TOKEN)[0]
 
-    def generate(self,
-                 prompt: str = STOP_TOKEN,
-                 num_return_sequences: int = 1,
-                 max_length: int = 20,
-                 temperature: float = 1.0,
-                 k: Optional[int] = None,
-                 p: float = 0.9,
-                 repetition_penalty: float = 1.0,
-                 stop_token: str = STOP_TOKEN):
-        max_length = adjust_length_to_model(max_length, max_sequence_length=self.model.config.max_position_embeddings)
-
-        encoded_prompt = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
-        encoded_prompt = encoded_prompt.to(self.device)
-
-        output_sequences = self.model.generate(
-            input_ids=encoded_prompt,
-            max_length=max_length + len(encoded_prompt[0]),
-            temperature=temperature,
-            top_k=k,
-            top_p=p,
-            repetition_penalty=repetition_penalty,
-            do_sample=True,
-            num_return_sequences=num_return_sequences,
-            pad_token_id=self.pad_token_id
-        )
-
-        # Remove the batch dimension when returning multiple sequences
-        if len(output_sequences.shape) > 2:
-            output_sequences.squeeze_()
-
-        return self._decode(output_sequences, encoded_prompt)
-
-    def _decode(self, output_sequences: List[torch.Tensor], encoded_prompt: Optional[torch.Tensor] = None):
-        generated_sequences = []
-
-        for generated_sequence_idx, generated_sequence in enumerate(output_sequences):
-            generated_sequence = generated_sequence.tolist()
-
-            # Decode text
-            text = self.tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
-
-            # Remove the excess text that was used for pre-processing
-            if encoded_prompt:
-                text = text[len(self.tokenizer.decode(encoded_prompt, clean_up_tokenization_spaces=True)):]
-
-            # Remove all text after the stop token
-            text = text[: text.find(self.STOP_TOKEN) if self.STOP_TOKEN else None]
-
-            generated_sequences.append(text)
-
-        return generated_sequences
+    # def generate(self,
+    #              prompt: str = STOP_TOKEN,
+    #              num_return_sequences: int = 1,
+    #              max_length: int = 20,
+    #              temperature: float = 1.0,
+    #              k: Optional[int] = None,
+    #              p: float = 0.9,
+    #              repetition_penalty: float = 1.0,
+    #              stop_token: str = STOP_TOKEN):
+    #     max_length = adjust_length_to_model(max_length, max_sequence_length=self.model.config.max_position_embeddings)
+    #
+    #     encoded_prompt = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
+    #     encoded_prompt = encoded_prompt.to(self.device)
+    #
+    #     output_sequences = self.model.generate(
+    #         input_ids=encoded_prompt,
+    #         max_length=max_length + len(encoded_prompt[0]),
+    #         temperature=temperature,
+    #         top_k=k,
+    #         top_p=p,
+    #         repetition_penalty=repetition_penalty,
+    #         do_sample=True,
+    #         num_return_sequences=num_return_sequences,
+    #         pad_token_id=self.pad_token_id
+    #     )
+    #
+    #     # Remove the batch dimension when returning multiple sequences
+    #     if len(output_sequences.shape) > 2:
+    #         output_sequences.squeeze_()
+    #
+    #     return self._decode(output_sequences, encoded_prompt)
+    #
+    # def _decode(self, output_sequences: List[torch.Tensor], encoded_prompt: Optional[torch.Tensor] = None):
+    #     generated_sequences = []
+    #
+    #     for generated_sequence_idx, generated_sequence in enumerate(output_sequences):
+    #         generated_sequence = generated_sequence.tolist()
+    #
+    #         # Decode text
+    #         text = self.tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
+    #
+    #         # Remove the excess text that was used for pre-processing
+    #         if encoded_prompt:
+    #             text = text[len(self.tokenizer.decode(encoded_prompt, clean_up_tokenization_spaces=True)):]
+    #
+    #         # Remove all text after the stop token
+    #         text = text[: text.find(self.STOP_TOKEN) if self.STOP_TOKEN else None]
+    #
+    #         generated_sequences.append(text)
+    #
+    #     return generated_sequences
 
     def generate_2(self):
-        model = GPT2LMHeadModel.from_pretrained('gpt2')
-        tokenizer = GPT2Tokenizer.from_pretrained('gpt2', pad_token='<PAD>')
+        # model = GPT2LMHeadModel.from_pretrained('gpt2')
+        # tokenizer = GPT2Tokenizer.from_pretrained('gpt2', pad_token='<PAD>')
         # IMPORTANT: Note that setting the <PAD> token like this itn the constructor gives the
         # pad_token the pad_token_id = 50256, which normally belongs to <BOS> token_ids in GPT2
         # This is a very ugly way that works at the moment of setting the pad_token_id to the <BOS> token that is already included in the vocab size. This will be updated in the coming weeks! # noqa: E501
@@ -113,7 +114,7 @@ class GPT2Generator:
         top_p = 0.
         temperature = 1.0
 
-        encodings_dict = tokenizer.batch_encode_plus(prompt_text, pad_to_max_length=True)
+        encodings_dict = self.tokenizer.batch_encode_plus(prompt_text, pad_to_max_length=True)
 
         # ideally we should be able to just input the following two variables to the function model.generate() ... => to be implemented soon!  # noqa: E501
         input_ids = torch.tensor(encodings_dict['input_ids'])
@@ -122,13 +123,13 @@ class GPT2Generator:
         batch_size, seq_len = input_ids.shape
 
         num_tokens_to_produce = 20
-        pad_token_id = tokenizer.pad_token_id
-        eos_token_id = tokenizer.eos_token_id
+        pad_token_id = self.tokenizer.pad_token_id
+        eos_token_id = self.tokenizer.eos_token_id
         eos_not_in_sents = torch.ones(input_ids.shape[0]).long()
 
         # we need to get the token ids of the last non-padded value
         last_non_masked_idx = torch.sum(attn_mask, dim=1) - 1
-        start_idx = inp_idx = (last_non_masked_idx).view(-1, 1).repeat(1, tokenizer.vocab_size).unsqueeze(1)
+        start_idx = inp_idx = (last_non_masked_idx).view(-1, 1).repeat(1, self.tokenizer.vocab_size).unsqueeze(1)
         past = None
 
         # get correct position ids
@@ -137,7 +138,7 @@ class GPT2Generator:
             position_ids_slice[last_non_masked_idx[i]:] = position_ids_slice[last_non_masked_idx[i]]
 
         for step in range(num_tokens_to_produce):
-            outputs = model(input_ids, attention_mask=attn_mask, position_ids=position_ids)
+            outputs = self.model(input_ids, attention_mask=attn_mask, position_ids=position_ids)
 
             # in the first decoding step, we want to use the 'real' last position for each sentence
             if step == 0:
@@ -170,13 +171,14 @@ class GPT2Generator:
             attn_mask = torch.cat([attn_mask, torch.ones((attn_mask.shape[0], 1)).long()], dim=1)
             position_ids = torch.cat([position_ids, (position_ids[:, -1] + 1).unsqueeze(-1)], dim=1)
 
-            [print(tokenizer.decode(output, skip_special_tokens=True)) for output in input_ids]
+        [print(self.tokenizer.decode(output, skip_special_tokens=True)) for output in input_ids]
 
 
-def test_generate():
-    generator = GPT2Generator()
-    generator.generate()
+# def test_generate():
+#     generator = GPT2Generator()
+#     generator.generate()
 
 
 def test_generate_2():
-    generate_2()
+    generator = GPT2Generator()
+    generator.generate_2()
