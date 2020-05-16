78ab616fd0947ab65a4ad50163d238931be5afc8
Sam Gehman <sgehman@cs.uw.edu>
2020-05-16 16:38:57 -0700

    Add jsonl utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from typing import TypeVar, Iterable, List, Sequence
+from typing import TypeVar, Iterable, List, Sequence, Union, Any
 
+import json
 import numpy as np
 import torch
 from tqdm.auto import tqdm
@@ -27,8 +28,8 @@ def batchify(data: Iterable[T], batch_size: int) -> Iterable[List[T]]:
         yield batch
 
 
-def save_gpt2_training_data(corpus: Sequence[str], out_file: Path, eos_token='<|endoftext|>'):
-    with out_file.open('a') as f:
+def save_gpt2_training_data(corpus: Sequence[str], out_file: Union[str, Path], eos_token='<|endoftext|>'):
+    with open(out_file, 'a') as f:
         for i, text in enumerate(tqdm(corpus, desc='Saving training data')):
             print(text, file=f, end='')
             if i != len(corpus) - 1:
@@ -44,3 +45,14 @@ def set_seed(seed, n_gpu):
 
 def load_text(filename: str) -> str:
     return (TEXTS_DIR / filename).read_text()
+
+
+def load_jsonl(file: Union[str, Path]) -> Iterable[Any]:
+    with open(file) as f:
+        for line in f:
+            yield json.loads(line)
+
+
+def write_jsonl(content: Any, file: Union[str, Path], mode='a'):
+    with open(file, mode) as f:
+        print(json.dumps(content), f)
