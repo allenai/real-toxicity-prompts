aa4fd1ad4126071fa833c95fb951d5d77a3c5f15
Sam Gehman <sgehman@cs.uw.edu>
2020-05-21 16:42:56 -0700

    Add pplm to run_eos_prompts.sh

--- scripts/run_eos_prompts.sh
+++ scripts/run_eos_prompts.sh
@@ -24,26 +24,34 @@ GEN_SAMPLES=10000
 #	--eos_prompt \
 #	output/prompts/eos/gpt2_ctrl_eos
 
-python -m scripts.run_prompts_experiments \
-	--model_type gpt2-affect \
-	--model_name_or_path /data/language-model-toxicity/models/gpt2-finetuned-models/affect_lm_doc_v1 \
-	--gen_batch_size 64 \
-	--gen_samples $GEN_SAMPLES \
-	--eos_prompt \
-	output/prompts/eos/gpt2_affect_beta1_eos
+#python -m scripts.run_prompts_experiments \
+#	--model_type gpt2-affect \
+#	--model_name_or_path /data/language-model-toxicity/models/gpt2-finetuned-models/affect_lm_doc_v1 \
+#	--gen_batch_size 64 \
+#	--gen_samples $GEN_SAMPLES \
+#	--eos_prompt \
+#	output/prompts/eos/gpt2_affect_beta1_eos
+#
+#python -m scripts.run_prompts_experiments \
+#	--model_type gpt2 \
+#	--model_name_or_path /data/language-model-toxicity/models/gpt2-finetuned-models/finetune_lte2_v2 \
+#	--gen_batch_size 64 \
+#	--gen_samples $GEN_SAMPLES \
+#	--eos_prompt \
+#	output/prompts/eos/gpt2_lte2_eos
+#
+#python -m scripts.run_prompts_experiments \
+#	--model_type gpt2 \
+#	--model_name_or_path /data/language-model-toxicity/models/gpt2-finetuned-models/finetune_gte99_v2 \
+#	--gen_batch_size 64 \
+#	--gen_samples $GEN_SAMPLES \
+#	--eos_prompt \
+#	output/prompts/eos/gpt2_gte99_eos
 
 python -m scripts.run_prompts_experiments \
-	--model_type gpt2 \
-	--model_name_or_path /data/language-model-toxicity/models/gpt2-finetuned-models/finetune_lte2_v2 \
-	--gen_batch_size 64 \
-	--gen_samples $GEN_SAMPLES \
+	--model_type pplm \
+	--model_name_or_path pplm \
+	--gen_batch_size 8 \
+	--gen_samples 1000 \
 	--eos_prompt \
-	output/prompts/eos/gpt2_lte2_eos
-
-python -m scripts.run_prompts_experiments \
-	--model_type gpt2 \
-	--model_name_or_path /data/language-model-toxicity/models/gpt2-finetuned-models/finetune_gte99_v2 \
-	--gen_batch_size 64 \
-	--gen_samples $GEN_SAMPLES \
-	--eos_prompt \
-	output/prompts/eos/gpt2_gte99_eos
+	output/prompts/eos/pplm_eos
