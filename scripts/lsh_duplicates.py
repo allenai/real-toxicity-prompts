c69323304233f8450e2caebbb0ed79f300f4669e
Sam Gehman <sgehman@cs.uw.edu>
2020-05-28 19:19:01 -0700

    Add OWTC support to lsh_duplicates.py

--- scripts/lsh_duplicates.py
+++ scripts/lsh_duplicates.py
@@ -1,17 +1,16 @@
+import os
 import multiprocessing as mp
 import pickle
 from functools import partial
+from pathlib import Path
 
+import click
 from datasketch import MinHash, LeanMinHash, MinHashLSH
 from nltk import ngrams
 from tqdm.auto import tqdm
 
-from utils.constants import DATA_DIR, OUTPUT_DIR
+from utils.constants import DATA_DIR
 from utils.utils import make_corpus_iter
-
-NUM_PERM = 128
-SHINGLES = 5
-JACCARD = 0.9
 
 
 def make_minhash_mapping(item, shingles: int, num_perm: int):
@@ -37,23 +36,55 @@ def parallel_create_minhashes(corpus_iter, shingles: int, num_perm: int, n_jobs:
         yield from pool.imap(make_minhash_mapping_, corpus_iter, chunksize=chunksize)
 
 
-def main():
-    wt_len = 8_282_020
-    wt_iter = make_corpus_iter(DATA_DIR / 'detokenized_webtext')
-    mh_iter = parallel_create_minhashes(wt_iter, shingles=SHINGLES, num_perm=NUM_PERM, n_jobs=96)
+@click.command()
+@click.option('--corpus', type=click.Choice(['webtext', 'openwebtext']))
+@click.option('--lsh/--no-lsh', default=False)
+@click.option('--num_perm', default=128)
+@click.option('--shingles', default=5)
+@click.option('--jaccard', default=0.9)
+@click.option('--n_jobs', default=os.cpu_count())
+@click.argument('output_dir', str)
+def main(corpus: str, lsh: bool, num_perm: int, shingles: int, jaccard: float, n_jobs: int, output_dir: str):
+    print("Making output dir:", output_dir)
+    output_dir = Path(output_dir)
+    output_dir.mkdir()
 
-    wt_minhashes = {}
-    wt_lsh = MinHashLSH(threshold=JACCARD, num_perm=NUM_PERM)
-    with wt_lsh.insertion_session() as session:
-        for key, minhash in tqdm(mh_iter, total=wt_len):
-            wt_minhashes[key] = minhash
-            session.insert(key, minhash, check_duplication=False)  # All keys are unique doc ids
+    if corpus == 'webtext':
+        corpus_len = 8_282_020
+        corpus_iter = make_corpus_iter(DATA_DIR / 'webtext_detokenized')
+    elif corpus == 'openwebtext':
+        corpus_len = 8_013_769
+        corpus_iter = make_corpus_iter(DATA_DIR / 'openwebtext_shards')
+    else:
+        raise RuntimeError
 
-    with open(OUTPUT_DIR / 'webtext_minhashes.pkl', 'wb') as f:
-        pickle.dump(wt_minhashes, f)
+    print("Starting", n_jobs, "threads for minhashing...")
+    minhash_iter = parallel_create_minhashes(corpus_iter, shingles=shingles, num_perm=num_perm, n_jobs=n_jobs)
+    minhashes = {}
 
-    with open(OUTPUT_DIR / 'webtext_lsh.pkl', 'wb') as f:
-        pickle.dump(wt_lsh, f)
+    if lsh:
+        print("Starting LSH session...")
+        lsh = MinHashLSH(threshold=jaccard, num_perm=num_perm)
+        with lsh.insertion_session() as session:
+            for key, minhash in tqdm(minhash_iter, total=corpus_len):
+                minhashes[key] = minhash
+                session.insert(key, minhash, check_duplication=False)  # All keys are unique doc ids
+
+        # Save LSH
+        print("Saving LSH...")
+        with open(output_dir / 'lsh.pkl', 'wb') as f:
+            pickle.dump(lsh, f)
+        print("Done saving LSH")
+    else:
+        print("Starting minhashing...")
+        for key, minhash in tqdm(minhash_iter, total=corpus_len):
+            minhashes[key] = minhash
+
+    # Save MinHashes
+    print("Saving MinHashes...")
+    with open(output_dir / 'minhashes.pkl', 'wb') as f:
+        pickle.dump(minhashes, f)
+    print("Done saving MinHashes")
 
 
 if __name__ == '__main__':
