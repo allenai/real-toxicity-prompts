84e37af7141a686fc8e179d84bae1988e5244db8
Sam Gehman <sgehman@cs.uw.edu>
2020-05-28 20:24:52 -0700

    Add query features to lsh_duplicates.py

--- scripts/lsh_duplicates.py
+++ scripts/lsh_duplicates.py
@@ -1,16 +1,21 @@
-import os
+import json
 import multiprocessing as mp
+import os
 import pickle
+import time
 from functools import partial
 from pathlib import Path
 
 import click
 from datasketch import MinHash, LeanMinHash, MinHashLSH
 from nltk import ngrams
-from tqdm.auto import tqdm
+from tqdm.auto import tqdm as _tqdm
 
 from utils.constants import DATA_DIR
 from utils.utils import make_corpus_iter
+
+# Default settings for tqdm
+tqdm = partial(_tqdm, dynamic_ncols=True)
 
 
 def make_minhash_mapping(item, shingles: int, num_perm: int):
@@ -37,14 +42,18 @@ def parallel_create_minhashes(corpus_iter, shingles: int, num_perm: int, n_jobs:
 
 
 @click.command()
-@click.option('--corpus', type=click.Choice(['webtext', 'openwebtext']))
-@click.option('--lsh/--no-lsh', default=False)
+@click.option('--corpus', type=click.Choice(['webtext', 'openwebtext']), required=True)
+@click.option('--mode', type=click.Choice(['lsh', 'query', 'minhash-only']), default='minhash-only')
+@click.option('--lsh_file', default=None, type=str)
 @click.option('--num_perm', default=128)
 @click.option('--shingles', default=5)
 @click.option('--jaccard', default=0.9)
 @click.option('--n_jobs', default=os.cpu_count())
 @click.argument('output_dir', type=str)
-def main(corpus: str, lsh: bool, num_perm: int, shingles: int, jaccard: float, n_jobs: int, output_dir: str):
+def main(corpus: str, mode: str, lsh_file: str, num_perm: int, shingles: int, jaccard: float, n_jobs: int,
+         output_dir: str):
+    assert mode == 'query' or not lsh_file
+
     print("Making output dir:", output_dir)
     output_dir = Path(output_dir)
     output_dir.mkdir()
@@ -62,29 +71,49 @@ def main(corpus: str, lsh: bool, num_perm: int, shingles: int, jaccard: float, n
     minhash_iter = parallel_create_minhashes(corpus_iter, shingles=shingles, num_perm=num_perm, n_jobs=n_jobs)
     minhashes = {}
 
-    if lsh:
-        print("Starting LSH session...")
+    print("Starting...")
+    if mode == 'lsh':
         lsh = MinHashLSH(threshold=jaccard, num_perm=num_perm)
         with lsh.insertion_session() as session:
-            for key, minhash in tqdm(minhash_iter, total=corpus_len):
+            for key, minhash in tqdm(minhash_iter, total=corpus_len, desc='Making MinHashLSH'):
                 minhashes[key] = minhash
                 session.insert(key, minhash, check_duplication=False)  # All keys are unique doc ids
 
         # Save LSH
         print("Saving LSH...")
+        start = time.time()
         with open(output_dir / 'lsh.pkl', 'wb') as f:
             pickle.dump(lsh, f)
-        print("Done saving LSH")
-    else:
-        print("Starting minhashing...")
-        for key, minhash in tqdm(minhash_iter, total=corpus_len):
+        print("Done saving LSH, time elapsed (sec):", time.time() - start)
+    elif mode == 'query':
+        print('Loading LSH:', lsh_file)
+        start = time.time()
+        with open(lsh_file, 'rb') as f:
+            lsh = pickle.load(f)
+        assert isinstance(lsh, MinHashLSH) and lsh.h == num_perm
+        print("Done loading LSH, time elapsed (sec):", time.time() - start)
+
+        duplicates_file = output_dir / 'duplicates.jsonl'
+        print("Writing duplicates to", duplicates_file)
+        with open(duplicates_file, 'a') as f:
+            for key, minhash in tqdm(minhash_iter, total=corpus_len, desc='Querying'):
+                minhashes[key] = minhash
+                duplicates = lsh.query(minhash)
+                if duplicates:
+                    json.dump({key: duplicates}, f)
+                    f.write('\n')
+    elif mode == 'minhash-only':
+        for key, minhash in tqdm(minhash_iter, total=corpus_len, desc='MinHashing'):
             minhashes[key] = minhash
+    else:
+        raise RuntimeError
 
     # Save MinHashes
     print("Saving MinHashes...")
+    start = time.time()
     with open(output_dir / 'minhashes.pkl', 'wb') as f:
         pickle.dump(minhashes, f)
-    print("Done saving MinHashes")
+    print("Done saving MinHashes, time elapsed (sec):", time.time() - start)
 
 
 if __name__ == '__main__':
