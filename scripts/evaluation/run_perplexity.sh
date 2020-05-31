3d78346f73ebc324fa62c2cb57b241aedb938f4d
Sam Gehman <sgehman@cs.uw.edu>
2020-05-31 00:47:14 -0700

    Update run_perplexity.sh

--- scripts/evaluation/run_perplexity.sh
+++ scripts/evaluation/run_perplexity.sh
@@ -12,39 +12,92 @@ CTRL_MODEL_PATH=$MODELS_DIR/gpt2_ctrl_v3_random_sample
 DAPT_NONTOXIC_PATH=$MODELS_DIR/finetune_lte2_v2
 DAPT_TOXIC_PATH=$MODELS_DIR/finetune_gte99_v2
 
-# 1. Affect on FULL DATA
+#############################
+# Affect
+#############################
 MODEL_PATH=$AFFECT_MODEL_PATH
 CONTROLLABLE=affect-gpt2
-BATCH_SIZE=$BATCH_SIZE
+
+# 1. Affect on FULL DATA
 EVAL_FILE=$WEBTEXT_TEST
 OUTPUT_DIR=output/eval/affect_gpt2_eval
-
-# 2. Affect on NONTOXIC
-MODEL_PATH=$AFFECT_MODEL_PATH
-CONTROLLABLE=affect-gpt2
-BATCH_SIZE=$BATCH_SIZE
-EVAL_FILE=$NONTOXIC_WEBTEXT_TEST
-OUTPUT_DIR=output/eval/affect_gpt2_nontoxic_eval
-
-# 3. CTRL-GPT2 on FULL DATA
-MODEL_PATH=$CTRL_MODEL_PATH
-CONTROLLABLE=ctrl-gpt2
-BATCH_SIZE=$BATCH_SIZE
-EVAL_FILE=WEBTEXT_TEST
-OUTPUT_DIR=output/eval/ctrl_gpt2_eval
-
-# 4. CTRL-GPT2 on NONTOXIC
-MODEL_PATH=$CTRL_MODEL_PATH
-CONTROLLABLE=ctrl-gpt2
-BATCH_SIZE=$BATCH_SIZE
-EVAL_FILE=$NONTOXIC_WEBTEXT_TEST
-OUTPUT_DIR=output/eval/ctrl_gpt2_nontoxic_eval
-
-# Run this command with different environment variables
 python -m scripts.evaluation.run_language_modeling_webtext \
   --model_type gpt2 \
   --model_name_or_path $MODEL_PATH \
   --controllable_model $CONTROLLABLE \
+  --do_eval \
+  --webtext \
+  --eval_data_file $EVAL_FILE \
+  --per_gpu_eval_batch_size $BATCH_SIZE \
+  --output_dir $OUTPUT_DIR
+
+# 2. Affect on NONTOXIC
+EVAL_FILE=$NONTOXIC_WEBTEXT_TEST
+OUTPUT_DIR=output/eval/affect_gpt2_nontoxic_eval
+python -m scripts.evaluation.run_language_modeling_webtext \
+  --model_type gpt2 \
+  --model_name_or_path $MODEL_PATH \
+  --controllable_model $CONTROLLABLE \
+  --do_eval \
+  --webtext \
+  --eval_data_file $EVAL_FILE \
+  --per_gpu_eval_batch_size $BATCH_SIZE \
+  --output_dir $OUTPUT_DIR
+
+############################
+# CTRL-GPT2
+############################
+MODEL_PATH=$CTRL_MODEL_PATH
+CONTROLLABLE=ctrl-gpt2
+
+# 3. CTRL-GPT2 on FULL DATA
+EVAL_FILE=$WEBTEXT_TEST
+OUTPUT_DIR=output/eval/ctrl_gpt2_eval
+python -m scripts.evaluation.run_language_modeling_webtext \
+  --model_type gpt2 \
+  --model_name_or_path $MODEL_PATH \
+  --controllable_model $CONTROLLABLE \
+  --do_eval \
+  --webtext \
+  --eval_data_file $EVAL_FILE \
+  --per_gpu_eval_batch_size $BATCH_SIZE \
+  --output_dir $OUTPUT_DIR
+
+# 4. CTRL-GPT2 on NONTOXIC
+EVAL_FILE=$NONTOXIC_WEBTEXT_TEST
+OUTPUT_DIR=output/eval/ctrl_gpt2_nontoxic_eval
+python -m scripts.evaluation.run_language_modeling_webtext \
+  --model_type gpt2 \
+  --model_name_or_path $MODEL_PATH \
+  --controllable_model $CONTROLLABLE \
+  --do_eval \
+  --webtext \
+  --eval_data_file $EVAL_FILE \
+  --per_gpu_eval_batch_size $BATCH_SIZE \
+  --output_dir $OUTPUT_DIR
+
+#############################
+# DAPT-NONTOXIC
+#############################
+MODEL_PATH=DAPT_NONTOXIC_PATH
+
+# 5. DAPT-NONTOXIC-GPT2 on FULL DATA
+EVAL_FILE=$WEBTEXT_TEST
+OUTPUT_DIR=output/eval/gpt2_dapt_lte2_eval
+python -m scripts.evaluation.run_language_modeling_webtext \
+  --model_type gpt2 \
+  --model_name_or_path $MODEL_PATH \
+  --do_eval \
+  --webtext \
+  --eval_data_file $EVAL_FILE \
+  --per_gpu_eval_batch_size $BATCH_SIZE \
+  --output_dir $OUTPUT_DIR
+
+EVAL_FILE=$NONTOXIC_WEBTEXT_TEST
+OUTPUT_DIR=output/eval/gpt2_dapt_lte2_nontoxic_eval
+python -m scripts.evaluation.run_language_modeling_webtext \
+  --model_type gpt2 \
+  --model_name_or_path $MODEL_PATH \
   --do_eval \
   --webtext \
   --eval_data_file $EVAL_FILE \
