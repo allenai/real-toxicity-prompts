ba1fe424d045f8d250125621cdff96fb97e7c3ed
Sam Gehman <sgehman@cs.uw.edu>
2020-05-30 23:34:24 -0700

    Clean up perplexity script

--- scripts/evaluation/run_perplexity.sh
+++ scripts/evaluation/run_perplexity.sh
@@ -1,37 +1,44 @@
 # CONSTANT environment variables
-WEBTEXT_TEST=x19_tokens.npy
-NONTOXIC_WEBTEXT_TEST=webtext_test_non_toxic.npy
-BATCH_SIZE=32
-AFFECT_MODEL_PATH=affect_doc_v1
-CTRL_MODEL_PATH=gpt2_ctrl_v3_random_sample
+SUBSAMPLES_DIR=/data/language-model-toxicity/data/webtext_subsamples
+MODELS_DIR=/data/language-model-toxicity/models/gpt2-finetuned-models
+
+WEBTEXT_TEST=$SUBSAMPLES_DIR/webtext_test_25_mil.npy
+NONTOXIC_WEBTEXT_TEST=$SUBSAMPLES_DIR/webtext_test_non_toxic_25_mil.npy
+
+BATCH_SIZE=4
+
+AFFECT_MODEL_PATH=$MODELS_DIR/affect_doc_v1
+CTRL_MODEL_PATH=$MODELS_DIR/gpt2_ctrl_v3_random_sample
+DAPT_NONTOXIC_PATH=$MODELS_DIR/finetune_lte2_v2
+DAPT_TOXIC_PATH=$MODELS_DIR/finetune_gte99_v2
 
 # 1. Affect on FULL DATA
 MODEL_PATH=$AFFECT_MODEL_PATH
 CONTROLLABLE=affect-gpt2
 BATCH_SIZE=$BATCH_SIZE
 EVAL_FILE=$WEBTEXT_TEST
-OUTPUT_DIR=output/eval/affect-gpt2-nontoxic
+OUTPUT_DIR=output/eval/affect_gpt2_eval
 
 # 2. Affect on NONTOXIC
 MODEL_PATH=$AFFECT_MODEL_PATH
 CONTROLLABLE=affect-gpt2
 BATCH_SIZE=$BATCH_SIZE
 EVAL_FILE=$NONTOXIC_WEBTEXT_TEST
-OUTPUT_DIR=output/eval/affect-gpt2-nontoxic
+OUTPUT_DIR=output/eval/affect_gpt2_nontoxic_eval
 
 # 3. CTRL-GPT2 on FULL DATA
 MODEL_PATH=$CTRL_MODEL_PATH
 CONTROLLABLE=ctrl-gpt2
 BATCH_SIZE=$BATCH_SIZE
 EVAL_FILE=WEBTEXT_TEST
-OUTPUT_DIR=output/eval/ctrl-gpt2
+OUTPUT_DIR=output/eval/ctrl_gpt2_eval
 
 # 4. CTRL-GPT2 on NONTOXIC
 MODEL_PATH=$CTRL_MODEL_PATH
 CONTROLLABLE=ctrl-gpt2
 BATCH_SIZE=$BATCH_SIZE
 EVAL_FILE=$NONTOXIC_WEBTEXT_TEST
-OUTPUT_DIR=output/eval/ctrl-gpt2-nontoxic
+OUTPUT_DIR=output/eval/ctrl_gpt2_nontoxic_eval
 
 # Run this command with different environment variables
 python -m scripts.evaluation.run_language_modeling_webtext \
