b66004e22cc4b3ac5b53a59a1cd0c5d5d417687b
Sam Gehman <sgehman@cs.uw.edu>
2020-06-02 12:35:39 -0700

    update bookscorpus script

--- scripts/data_preprocessing/bookscorpus_jsonl.py
+++ scripts/data_preprocessing/bookscorpus_jsonl.py
@@ -1,16 +1,19 @@
 import json
 
-from utils.utils import load_jsonl
+from utils.utils import load_jsonl, first
 from utils.perspective_api import unpack_scores
 
 
 def save_jsonl(perspective_file, corpus_file, out_file):
-    with open(corpus_file) as f_in, open(out_file, 'a') as f_out:
-        for text, response in zip(f_in, load_jsonl(perspective_file)):
+    with open(out_file, 'a') as f_out:
+        for text, response in zip(load_jsonl(corpus_file), load_jsonl(perspective_file)):
             if not response['success']:
                 continue
 
-            id_ = int(response['request_id'])
+            id_, text = first(text.items())
+            assert id_ == response['request_id']
+            id_ = int(id_)
+
             summary_scores, span_scores = unpack_scores(response['response'])
             json.dump({'line': id_, 'text': text, **summary_scores}, f_out)
             f_out.write('\n')
