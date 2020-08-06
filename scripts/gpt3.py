a2f7a57bf3f96403c8ec3b03955f2ce424196e7e
Sam Gehman <sgehman@cs.uw.edu>
2020-08-06 14:35:48 -0700

    Clean up gpt3.py

--- scripts/gpt3.py
+++ scripts/gpt3.py
@@ -1,52 +1,48 @@
-from math import ceil
-from utils.constants import OPENAI_API_KEY
-import openai
-from pathlib import Path
-import pandas as pd
 import json
+from pathlib import Path
+from typing import List
+
 import click
+import openai
+import pandas as pd
+from math import ceil
 from tqdm.auto import tqdm, trange
 
+from utils.constants import OPENAI_API_KEY
 from utils.utils import batchify
 
 openai.api_key = OPENAI_API_KEY
 
+EOS = ''
 PROMPT_BATCH_SIZE = 1
 ENGINE = 'davinci'
 MAX_TOKENS = 20
 N = 20
 
 
+def request(prompt: str):
+    # Retry request (handles connection errors, timeouts, and overloaded API)
+    while True:
+        try:
+            return openai.Completion.create(
+                engine=ENGINE,
+                prompt=prompt,
+                max_tokens=MAX_TOKENS,
+                n=N
+            )
+        except Exception as e:
+            tqdm.write(str(e))
+            tqdm.write("Retrying...")
+
+
 def generate_eos(num_samples: int):
     for _ in trange(ceil(num_samples / N)):
-        while True:
-            try:
-                yield openai.Completion.create(
-                    engine=ENGINE,
-                    prompt='',
-                    max_tokens=MAX_TOKENS,
-                    n=N
-                )
-                break
-            except openai.error.APIError as e:
-                tqdm.write(str(e))
-                tqdm.write("Retrying...")
+        yield request(EOS)
 
 
-def generate(prompts):
+def generate_prompted(prompts: List[str]):
     for prompt in tqdm(batchify(prompts, PROMPT_BATCH_SIZE), total=ceil(len(prompts) / PROMPT_BATCH_SIZE)):
-        while True:
-            try:
-                yield openai.Completion.create(
-                    engine=ENGINE,
-                    prompt=prompt,
-                    max_tokens=MAX_TOKENS,
-                    n=N
-                )
-                break
-            except Exception as e:
-                tqdm.write(str(e))
-                tqdm.write("Retrying...")
+        yield request(prompt)
 
 
 def write(response, fp):
@@ -75,7 +71,7 @@ def main(prompts_file: str, num_eos_samples: int, out_file: str):
             print("Loading prompts from", prompts_file)
             df = pd.read_csv(prompts_file)
             prompts = df['prompt.text']
-            for gen in generate(prompts[num_skip:]):
+            for gen in generate_prompted(prompts[num_skip:]):
                 write(gen, f)
         else:
             print("Using EOS as prompt")
